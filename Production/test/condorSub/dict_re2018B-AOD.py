9acdf005617dc853376d132139f9c9e92b525264
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-02-27 18:52:43 +0100

    METAOD for 2018B

--- Production/test/condorSub/dict_re2018B-AOD.py
+++ Production/test/condorSub/dict_re2018B-AOD.py
@@ -4,4 +4,5 @@ flist = {'scenario':
 [
 ['Run2018B-17Sep2018-v1.JetHTAOD'],
 ['Run2018B-17Sep2018-v1.SingleMuonAOD']
+['Run2018B-17Sep2018-v1.METAOD']
 ]}
--- Production/test/run_tests.sh
+++ Production/test/run_tests.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# let's test all data scenarios with miniAOD re-creation for TreeMaker
-
 echo "test Run2016C rereco"
 rm info_*
 cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2016MiniAODv3 inputFilesConfig=Run2016C-17Jul2018-v1.METAOD nstart=5 nfiles=1 outfile=test numevents=10
 ./get_miniAOD.py --infile="$(cat info_aodfilename)" --outfile=miniaod.root --nev="$(cat info_nev)"
 cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2016MiniAODv3 inputFilesConfig=Run2016C-17Jul2018-v1.METAOD nstart=5 nfiles=1 outfile=test numevents=10
 if [[ $? -ne 0 ]]; then echo "Failed test"; else echo "Success"; fi
-
-echo "test Run2016G rereco"
-
-if [[ $? -ne 0 ]]; then echo "Failed test"; else echo "Success"; fi
-
+# OK!
 
 echo "test Run2017 rereco"
 rm info_*
@@ -20,7 +14,7 @@ cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2017ReReco31Mar inputFilesConfig=R
 ./get_miniAOD.py --infile="$(cat info_aodfilename)" --outfile=miniaod.root --nev="$(cat info_nev)"
 cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2017ReReco31Mar inputFilesConfig=Run2017E-31Mar2018-v1.METAOD nstart=0 nfiles=1 outfile=test numevents=10
 if [[ $? -ne 0 ]]; then echo "Failed test"; else echo "Success"; fi
-# OK!
+# OK! ret
 
 echo "test Run2018 rereco"
 rm info_*
@@ -28,7 +22,7 @@ cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2018ReReco17Sep inputFilesConfig=R
 ./get_miniAOD.py --infile="$(cat info_aodfilename)" --outfile=miniaod.root --nev="$(cat info_nev)"
 cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=2018ReReco17Sep inputFilesConfig=Run2018A-17Sep2018-v1.METAOD nstart=0 nfiles=1 outfile=Run2018A-17Sep2018-v1.METAOD numevents=10
 if [[ $? -ne 0 ]]; then echo "Failed test"; else echo "Success"; fi
-# OK!
+# OK! ret
 
 echo "test Run2018 rereco"
 rm info_*
