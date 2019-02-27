9ac5fd88ec62271c1818d0e96557a09f13a741e5
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-02-27 16:22:25 +0100

    updated get_miniAOD.py

--- Production/test/get_miniAOD.py
+++ Production/test/get_miniAOD.py
@@ -14,6 +14,43 @@ print 'locating miniAOD file(s) which match the AOD file:', options.infile
 if not 'root://' in options.infile:
     options.infile = 'root://cmsxrootd.fnal.gov/' + options.infile
 
+#get corresponding miniAOD file(s): 
+miniaod_filenames = []
+status, parent_filenames = commands.getstatusoutput('dasgoclient -query="parent file=/%s"' % options.infile.split("//")[-1])
+status, child_filenames = commands.getstatusoutput('dasgoclient -query="child file=/%s"' % options.infile.split("//")[-1])
+
+for parent_filename in parent_filenames.split("\n"):
+    status, cousins_filenames = commands.getstatusoutput('dasgoclient -query="child file=%s"' % parent_filename)
+
+    relatives_filenames = cousins_filenames.split("\n") + child_filenames.split("\n")
+
+    for relative in relatives_filenames:
+        if   "Run2016B" in options.infile and "/MINIAOD/17Jul2018_ver1-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016B" in options.infile and "/MINIAOD/17Jul2018_ver2-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016B" in options.infile and "/MINIAOD/17Jul2018_ver2-v2/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016C" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016D" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016E" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016F" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016G" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016H" in options.infile and "/MINIAOD/17Jul2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2016H" in options.infile and "/MINIAOD/17Jul2018-v2/" in relative: miniaod_filenames.append(relative)
+        elif "Run2017"  in options.infile and "/MINIAOD/31Mar2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2018A" in options.infile and "/MINIAOD/17Sep2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2018B" in options.infile and "/MINIAOD/17Sep2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2018C" in options.infile and "/MINIAOD/17Sep2018-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2018D" in options.infile and "/MINIAOD/PromptReco-v1/" in relative: miniaod_filenames.append(relative)
+        elif "Run2018D" in options.infile and "/MINIAOD/PromptReco-v2/" in relative: miniaod_filenames.append(relative)
+
+    if len(miniaod_filenames) == 0:
+        print "Could not locate corresponding miniAOD file!"
+        quit(99)
+
+miniaod_filenames = list(set(miniaod_filenames))
+print "miniaod_filenames", miniaod_filenames
+os.system("echo %s > info_miniaods" % ",".join(miniaod_filenames))
+
+
 #first, get start / end of AOD file:
 print "Running edmFileUtil to get run number / lumisec information..."
 os.system("edmFileUtil %s -e > TMPFILE" % options.infile)
@@ -25,59 +62,12 @@ run_stop = output.split()[0]
 lumi_stop = output.split()[1]
 aod_range = "%s:%s-%s:%s" % (run_start, lumi_start, run_stop, lumi_stop)
 
-#get corresponding miniAOD file(s): 
-miniaod_filenames = []
-status, parent_filenames = commands.getstatusoutput('dasgoclient -query="parent file=/%s"' % options.infile.split("//")[-1])
-for parent_filename in parent_filenames.split("\n"):
-    status, children = commands.getstatusoutput('dasgoclient -query="child file=%s"' % parent_filename)
-    for child in children.split("\n"):
-        if "/MINIAOD/" in child and "17Sep2018-v1" in child:
-            miniaod_filenames.append(child)
 
-miniaod_filenames = list(set(miniaod_filenames))
-print "miniaod_filenames", miniaod_filenames
-
-os.system("echo %s > info_miniaods" % ",".join(miniaod_filenames))
-
+# now, do the union of golden json and custom json:
 json_content = '{"%s": [[%s, %s]]}' % (run_start, lumi_start, lumi_stop)
 with open("lumisecs.json", "w") as fo:
     fo.write(json_content)
-
-
-#os.system("echo %s > lumisecs.json" % json_content)
 os.system("compareJSON.py --and $(cat info_jsonfilename) lumisecs.json > lumisecs_union.json")
 
 
-
-
-#IGNORE
-
-pyset = """
-import FWCore.ParameterSet.Config as cms
-process = cms.Process('NoSplit')
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(%s),
-                            lumisToProcess = cms.untracked.VLuminosityBlockRange('%s'),
-                    )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(%s))
-#process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
-process.output = cms.OutputModule("PoolOutputModule",
-    #outputCommands = cms.untracked.vstring("drop *", "keep *_*RecHits*_*_*", "keep *_*dedx*Harmonic*_*_*"),
-    outputCommands = cms.untracked.vstring("keep *"),
-    fileName = cms.untracked.string('%s'),
-)
-process.out = cms.EndPath(process.output)
-""" % (",".join(miniaod_filenames), aod_range, options.nev, options.outfile)
-
-#print pyset
-#with open("pyset.py", "w") as fo:
-#    fo.write(pyset)
-
-#status, output = commands.getstatusoutput("grep 'events' TMPFILE")
-#print "stats:\n", output
-
-#print "running cmsRun..."
-#status, output = commands.getstatusoutput("cmsRun pyset.py")
-#print output
+print "Done"
