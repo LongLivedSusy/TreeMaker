a9fc8fa4605cd77d310b96535bd64c77a17befce
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-04-26 15:39:39 +0200

    added log file cleanup script

--- Production/test/condorSub/cleanup.sh
+++ Production/test/condorSub/cleanup.sh
@@ -1,1 +1,8 @@
-x
+#!/bin/sh
+find . -name "*.condor" -delete
+find . -name "*stdout" -delete
+find . -name "*stderr" -delete
+find . -name "jobExecCondor_*jdl" -delete
+rm *root *.tar.gz
+rm -rf input
+mkdir input
