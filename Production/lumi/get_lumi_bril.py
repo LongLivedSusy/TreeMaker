a1eb94f87f8e42ac857995e5bc75d057fa53c17d
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-05-09 18:07:50 +0200

    cleanup

--- Production/lumi/get_lumi_bril.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-import glob
-import os
-
-for json in glob.glob("*.json"):
-    os.system("brilcalc lumi -u /fb -i %s > %s" % (json, json + ".briloutput"))   
-
-os.system("mkdir -p processed_jsons")
-os.system("cp *json processed_jsons/")
