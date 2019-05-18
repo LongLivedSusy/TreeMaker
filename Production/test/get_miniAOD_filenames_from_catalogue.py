b02d0800187f232d60b312a35b960b64b5197b62
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-05-18 16:17:18 +0200

    added redirector

--- Production/test/get_miniAOD_filenames_from_catalogue.py
+++ Production/test/get_miniAOD_filenames_from_catalogue.py
@@ -56,7 +56,12 @@ def get_miniAOD_filenames(aod_file_name):
     
     #first, get start / end of AOD file:
     print "Running edmFileUtil to get run number / lumisec information..."
-    os.system("edmFileUtil %s -e > TMPFILE" % aod_file_name)
+    if aod_file_name[0:7] == "/store/":
+        print "edmFileUtil root://cmsxrootd.fnal.gov/%s -e > TMPFILE" % aod_file_name
+        os.system("edmFileUtil root://cmsxrootd.fnal.gov/%s -e > TMPFILE" % aod_file_name)
+    else:
+        print "edmFileUtil %s -e > TMPFILE" % aod_file_name
+        os.system("edmFileUtil %s -e > TMPFILE" % aod_file_name)
 
     #write json file for AOD file:
     runs = collections.OrderedDict()
