f9cc4574706ebeeae13043108365ffc44f705ff9
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-06-03 15:06:23 +0200

    added HT5-50 DY Summer16 samples for Yuval

---
+++ Production/test/condorSub/dict_RunIISummer16MiniAODv3_dyjets_yuval-AOD.py
@@ -0,0 +1,10 @@
+flist = {
+    "scenario": "Summer16MiniAODv3",
+    "samples": [
+        ['RunIISummer16MiniAODv3.DYJetsToLL_M-5to50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIISummer16MiniAODv3.DYJetsToLL_M-5to50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIISummer16MiniAODv3.DYJetsToLL_M-5to50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIISummer16MiniAODv3.DYJetsToLL_M-5to50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIISummer16MiniAODv3.DYJetsToLL_M-5to50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-AOD'],
+    ]
+}
