1da0d1af6676cd24e082563654f1273ce38a46be
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-06-08 21:34:18 +0200

    submitJobs.py

--- Production/test/condorSub/submitJobs.py
+++ Production/test/condorSub/submitJobs.py
@@ -1,8 +1,13 @@
 from jobSubmitterTM import jobSubmitterTM
+import os
 
 def submitJobs():  
     mySubmitter = jobSubmitterTM()
     mySubmitter.run()
     
 if __name__=="__main__":
+    
+    print "Updating condor submission scripts..."
+    os.system("cd ..; ./update_custom_submission_scripts.sh; cd condorSub")
+    
     submitJobs()
