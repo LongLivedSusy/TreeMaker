8afb08cd6b11ded06ac63769045396b17e66108f
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-07-12 11:39:22 +0200

    get gridpack via standard method

--- Production/test/update_custom_submission_scripts.sh
+++ Production/test/update_custom_submission_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp $CMSSW_BASE/src/TreeMaker/Production/aod_support/step1.sh $CMSSW_BASE/src/Condor/Production/scripts/step1.sh
-cp $CMSSW_BASE/src/TreeMaker/Production/aod_support/checkVomsTar.sh $CMSSW_BASE/src/Condor/Production/scripts/checkVomsTar.sh
+#cp $CMSSW_BASE/src/TreeMaker/Production/aod_support/step1.sh $CMSSW_BASE/src/Condor/Production/scripts/step1.sh
+#cp $CMSSW_BASE/src/TreeMaker/Production/aod_support/checkVomsTar.sh $CMSSW_BASE/src/Condor/Production/scripts/checkVomsTar.sh
