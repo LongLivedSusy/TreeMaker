1fdf8fe4ea4b1feeb822e0ae54127e40e9a89997
Viktor Kutzner <viktor.kutzner@cern.ch>
2020-09-13 01:32:11 +0200

    wip

---
+++ Production/test/condorSub/dict_RunIIFall17MiniAODv2_signalFastSim.py
@@ -0,0 +1,9 @@
+flist = {
+    "scenario": "Fall17Fastsig",
+    "samples": [
+        ['RunIIFall17MiniAODv2.FastSim-SMS-T1qqqq-LLChipm_ctau-10_TuneCP2_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIIFall17MiniAODv2.FastSim-SMS-T1qqqq-LLChipm_ctau-50_TuneCP2_13TeV-madgraphMLM-pythia8ext1-AOD'],
+        ['RunIIFall17MiniAODv2.FastSim-SMS-T1qqqq-LLChipm_ctau-200_TuneCP2_13TeV-madgraphMLM-pythia8-AOD'],
+        ['RunIIFall17MiniAODv2.FastSim-SMS-T1qqqq-LLChipm_ctau-200_TuneCP2_13TeV-madgraphMLM-pythia8ext1-AOD'],
+    ]
+}
