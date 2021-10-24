3f292188b483ad6131be06169fc9d9d5f91e34cc
Viktor Kutzner <viktor.kutzner@cern.ch>
2021-10-24 23:33:18 +0200

    added 2018 pmssm signals

---
+++ Production/test/condorSub/dict_pmssm.py
@@ -0,0 +1,10 @@
+flist = {
+    "scenario": "Autumn18sigFS",
+    "samples": [
+        ['RunIIAutumn18FS.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8-AOD0'],
+        ['RunIIAutumn18FS.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8-AOD1'],
+        ['RunIIAutumn18FS.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8-AOD2'],
+        ['RunIIAutumn18FS.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8-AOD3'],
+        ['RunIIAutumn18FS.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8-AOD4'],
+    ]
+}
