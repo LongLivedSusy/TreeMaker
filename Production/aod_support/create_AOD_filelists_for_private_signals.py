55e41c147651e87ccd025aa6e732fee9b3c4fa04
Viktor Kutzner <viktor.kutzner@cern.ch>
2021-11-30 00:26:51 +0100

    wip

--- Production/aod_support/create_AOD_filelists_for_private_signals.py
+++ Production/aod_support/create_AOD_filelists_for_private_signals.py
@@ -12,50 +12,44 @@ def dochunks(l, n):
         # Create an index range for l of n items:
         yield l[i:i+n]
 
-#miniAOD_path = "/nfs/dust/cms/user/kutznerv/DisappTrksSignalMC/CMSSW10/miniAODSIM/"
-#pyfilename = "../python/PrivateSamples/g1800_chi1400_27_200970_CMSSW10_cff.py"
 
-#miniAOD_path = "/nfs/dust/cms/user/kutznerv/DisappTrksSignalMC/CMSSW8/miniAODSIM/"
-#pyfilename = "../python/PrivateSamples/g1800_chi1400_27_200970_CMSSW8_cff.py"
-
-#miniAOD_path = "/nfs/dust/cms/user/beinsam/LongLiveTheChi/Production/FastScans/step3_test_higgsino_susyall_mChipm105GeV_dm0p36GeV_pu_inMINIAODSIM.root"
-#pyfilename = "../python/PrivateSamples/sam_fasthiggsino.py"
-
-#miniAOD_path = "/nfs/dust/cms/user/beinsam/CommonSamples/MC_BSM/CompressedHiggsino/RadiativeMu/"
-#pyfilename = "../python/PrivateSamples/sam_CompressedHiggsino.py"
-
-#miniAOD_path = "/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18.SMS-T1btbt-LLChipm/*/*/*/*.root"
-#pyfilename = "../python/PrivateSamples/sam_resms-T1btbt-LLChipm-AOD.py"
-
-miniAOD_path = "/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18.SMS-T2bt-LLChipm/*/*/*/*.root"
-pyfilename = "../python/PrivateSamples/sam_resms-T2bt-LLChipm-AOD.py"
-
-#miniAOD_path = "/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18.SMS-T2tb-LLChipm/*/*/*/*.root"
-#pyfilename = "../python/PrivateSamples/sam_resms-T2tb-LLChipm-AOD.py"
-
-#all_filenames = natsorted(glob.glob(miniAOD_path + "/step3_higgsino_susyall*MINIAODSIM.root"))
-all_filenames = natsorted(glob.glob(miniAOD_path))
-
-chunks = list(dochunks(all_filenames, 254))
-
-with open(pyfilename, "w+") as fout:
-    header = """import FWCore.ParameterSet.Config as cms
-
-maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-readFiles = cms.untracked.vstring()
-secFiles = cms.untracked.vstring()
-source = cms.Source("PoolSource", fileNames = readFiles, secondaryFileNames = secFiles)
-"""
-
-    fout.write(header)
-
-    for chunk in chunks:
-        fout.write("readFiles.extend( [\n")
-        for ifile in chunk:
-            if ifile != "":
-                fout.write("       'file://%s',\n" % ifile)
-        fout.write("] )\n")
-        
-print "%s written!" % pyfilename
+def create_filelist(miniAOD_path, pyfilename):
+    
+    #all_filenames = natsorted(glob.glob(miniAOD_path + "/step3_higgsino_susyall*MINIAODSIM.root"))
+    all_filenames = natsorted(glob.glob(miniAOD_path))
+    
+    chunks = list(dochunks(all_filenames, 254))
+    
+    with open(pyfilename, "w+") as fout:
+        header = """import FWCore.ParameterSet.Config as cms
+    
+    maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+    readFiles = cms.untracked.vstring()
+    secFiles = cms.untracked.vstring()
+    source = cms.Source("PoolSource", fileNames = readFiles, secondaryFileNames = secFiles)
+    """
+    
+        fout.write(header)
+    
+        for chunk in chunks:
+            fout.write("readFiles.extend( [\n")
+            for ifile in chunk:
+                if ifile != "":
+                    fout.write("       'file://%s',\n" % ifile)
+            fout.write("] )\n")
+            
+    print "%s written!" % pyfilename
 
 
+#create_filelist("/nfs/dust/cms/user/kutznerv/DisappTrksSignalMC/CMSSW10/miniAODSIM/", "../python/PrivateSamples/g1800_chi1400_27_200970_CMSSW10_cff.py")
+#create_filelist("/nfs/dust/cms/user/kutznerv/DisappTrksSignalMC/CMSSW8/miniAODSIM/", "../python/PrivateSamples/g1800_chi1400_27_200970_CMSSW8_cff.py")
+#create_filelist("/nfs/dust/cms/user/beinsam/LongLiveTheChi/Production/FastScans/step3_test_higgsino_susyall_mChipm105GeV_dm0p36GeV_pu_inMINIAODSIM.root", "../python/PrivateSamples/sam_fasthiggsino.py")
+#create_filelist("/nfs/dust/cms/user/beinsam/CommonSamples/MC_BSM/CompressedHiggsino/RadiativeMu/", "../python/PrivateSamples/sam_CompressedHiggsino.py")
+
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18.SMS-T1btbt-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIAutumn18-T1btbt-LLChipm-AOD_cff.py")
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18.SMS-T2bt-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIAutumn18-T2bt-LLChipm-AOD_cff.py")
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIAutumn18__SMS-T2tb-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIAutumn18-T2tb-LLChipm-AOD_cff.py")
+
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIFall17__SMS-T1btbt-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIFall17-T1btbt-LLChipm-AOD_cff.py")
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIFall17__SMS-T2bt-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIFall17-T2bt-LLChipm-AOD_cff.py")
+create_filelist("/pnfs/desy.de/cms/tier2/store/user/sbein/SMS2/RunIIFall17__SMS-T2tb-LLChipm/*/*/*/*.root", "../python/PrivateSamples/sam_RunIIFall17-T2tb-LLChipm-AOD_cff.py")
---
+++ Production/aod_support/submit_to_local_condor_RunIIAutumn18FS.py
@@ -0,0 +1,64 @@
+#!/bin/env python
+import os, glob
+from GridEngineTools import runParallel
+import commands
+
+campaigns = { 
+              "PrivateSamples": {
+                                  "scenario": "Autumn18Fastsig",
+                                  "cmsswpath": "/nfs/dust/cms/user/kutznerv/shorttrack/treemaker/CMSSW_10_2_7/src",
+                                  "inputFilesConfigs": [
+                                                     "sam_RunIIAutumn18-T1btbt-LLChipm-AOD",
+                                                     "sam_RunIIAutumn18-T2bt-LLChipm-AOD",
+                                                     "sam_RunIIAutumn18-T2tb-LLChipm-AOD",
+                                                     ]
+                                 },
+              }
+
+cmds = []
+output_folder = "srm://dcache-se-cms.desy.de/pnfs/desy.de/cms/tier2/store/user/vkutzner/NtupleHub/ProductionRun2v3/"
+
+for campaign in campaigns:
+    print campaign
+    for inputFilesConfig in campaigns[campaign]["inputFilesConfigs"]:
+        print inputFilesConfig
+        
+        status, file_names = commands.getstatusoutput("grep .root ../python/%s/%s_cff.py | grep '#' -v " % (campaign, inputFilesConfig))
+        file_names = file_names.split("\n")
+        this_inputFilesConfig = "%s.%s" % (campaign, inputFilesConfig)
+        
+        for i, file_name in enumerate(file_names):
+
+            if "'" not in file_name: continue
+
+            file_name = file_name.split("'")[1]
+            file_name = "-".join(file_name.split("/")[-5:])
+            file_name = file_name.replace(".root", "")
+            file_name = file_name.replace("RunIIAutumn18.", "RunIIAutumn18FSv2.")
+
+            cmsrun = """source /cvmfs/cms.cern.ch/cmsset_default.sh; cd /afs/desy.de/user/k/kutznerv/dust/shorttrack/treemaker/CMSSW_10_2_7/src/TreeMaker/Production/test/;
+                      eval `scramv1 runtime -sh`;
+                      cmsRun runMakeTreeFromMiniAOD_cfg.py scenario=%s inputFilesConfig=%s nstart=%s nfiles=1 outfile=%s;
+                      eval `scram unsetenv -sh`;
+                      source /cvmfs/grid.desy.de/etc/profile.d/grid-ui-env.sh;
+                      gfal-copy -f %s_RA2AnalysisTree.root %s;
+                      rm %s_RA2AnalysisTree.root""" % (campaigns[campaign]["scenario"], this_inputFilesConfig, i, file_name, file_name, output_folder, file_name)
+            
+            # check if outputfile already exists...
+            if not os.path.exists("%s/%s_RA2AnalysisTree.root" % (output_folder.replace("srm://dcache-se-cms.desy.de", ""), file_name)):
+                cmds.append(cmsrun.replace("\n", ""))
+
+print "There are %s files to process" % len(cmds)
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+        
+cmds = list(chunks(cmds, 25))
+for i in range(len(cmds)):
+    cmds[i] = "; ".join(cmds[i])
+        
+#runParallel(cmds, "multi", use_sl6=True, condorDir="condor2018FSSam")
+runParallel(cmds, "grid", use_sl6=False, condorDir="condor2018FSSam")
+
