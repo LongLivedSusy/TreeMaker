aa2f1eda3347865cbeca853b692ac7ddbcfc8a9c
sbein <Samuel.bein@gmail.com>
2022-02-02 06:24:03 +0100

    adding SUS PAG- (and Kevin)-approved FastSim event weight to be used for samples produced with buggy decays

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -219,12 +219,16 @@ def makeTreeFromMiniAOD(self,process):
         self.VectorBool.append("genParticles:TTFlag(GenParticles_TTFlag)")
         self.VectorInt.append("genParticles:LabXYmm(GenParticles_LabXYmm)")
 
-        process.pmssmEventMask = cms.EDProducer("PmssmEventMaskProducer",
-            genCollection = cms.InputTag("prunedGenParticles"),
-        )
         # store gluons for signals with Higgs
-        self.VarsBool.append("pmssmEventMask:EventMask(Pmssm_EventMask)")
-        self.VarsBool.append("pmssmEventMask:EventMaskRoyal(PmssmEventMaskRoyal)")            
+        
+        #Event weight to fix FastSim pre-UL bug
+        if self.fastsim:
+            process.FastSimWeightPR31285To36122 = cms.EDProducer("FastSimWeightPR31285To36122",
+                genCollection = cms.InputTag("prunedGenParticles"),
+                genJetTag = cms.InputTag('slimmedGenJets'),
+                recJetTag = cms.InputTag("slimmedJets")
+            )
+            self.VarsDouble.append("FastSimWeightPR31285To36122")
 
         #GEANT4:
         if self.signal and not self.fastsim:
