c8223762a41c8df9ca8fbc2aa356a235b2106cd5
Viktor Kutzner <viktor.kutzner@cern.ch>
2022-05-19 16:17:16 +0200

    hotfix

--- Production/test/job_main_loop.py
+++ Production/test/job_main_loop.py
@@ -82,13 +82,13 @@ if __name__ == "__main__":
     # redo miniAODs for 2018D MET (due to corrupt miniAODs present):
     if "2018D" in aod_files[0] and "MET" in aod_files[0]:
         redo_miniaod = True
-    if "2018" in aod_file[0] and "MET" in aod_files[0]:
+    if "2018" in aod_files[0] and "MET" in aod_files[0]:
         redo_miniaod = True
-    if "2018D" in aod_file[0] and "JetHT" in aod_file[0]:
+    if "2018D" in aod_files[0] and "JetHT" in aod_files[0]:
         redo_miniaod = True
-    if "2018D" in aod_file[0] and "SingleMuon" in aod_file[0]:
+    if "2018D" in aod_files[0] and "SingleMuon" in aod_files[0]:
         redo_miniaod = True
-    if "EGamma" in aod_file[0]:
+    if "EGamma" in aod_files[0]:
         redo_miniaod = True
 
     if redo_miniaod:
