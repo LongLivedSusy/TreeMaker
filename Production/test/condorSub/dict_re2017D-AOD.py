b6e3941b3238a455d7c65a293bed3bbe26b19303
Viktor Kutzner <viktor.kutzner@cern.ch>
2022-12-19 23:54:31 +0100

    updated dicts

--- Production/test/condorSub/dict_re2017D-AOD.py
+++ Production/test/condorSub/dict_re2017D-AOD.py
@@ -6,8 +6,8 @@ flist = {'scenario':
 ['Run2017D-31Mar2018-v1.METAOD'],
 ['Run2017D-31Mar2018-v1.SingleElectronAOD'],
 ['Run2017D-31Mar2018-v1.SingleMuonAOD'],
-['Run2017D-17Jul2018-v1.JetHTAOD90'],
-['Run2017D-17Jul2018-v1.METAOD90'],
-['Run2017D-17Jul2018-v1.SingleElectronAOD90'],
-['Run2017D-17Jul2018-v1.SingleMuonAOD90'],
+['Run2017D-31Mar2018-v1.JetHTAOD90'],
+['Run2017D-31Mar2018-v1.METAOD90'],
+['Run2017D-31Mar2018-v1.SingleElectronAOD90'],
+['Run2017D-31Mar2018-v1.SingleMuonAOD90'],
 ]}
