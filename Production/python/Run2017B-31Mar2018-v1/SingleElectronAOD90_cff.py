687bbb94ee03fd5a2e75158dd8a48ed06a827821
Viktor Kutzner <viktor.kutzner@cern.ch>
2023-01-22 23:11:50 +0100

    updated filelists

--- Production/python/Run2017B-31Mar2018-v1/SingleElectronAOD90_cff.py
+++ Production/python/Run2017B-31Mar2018-v1/SingleElectronAOD90_cff.py
@@ -8,7 +8,7 @@ readFiles.extend( [
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/50000/E458DB87-F2DD-E711-AA37-02163E019E83.root',
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/70000/4AEB8D0C-09DE-E711-B76D-02163E01347A.root',
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/60000/F8ED2704-86E2-E711-964E-02163E019B9C.root',
-       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/40000/C0EC07D4-D9DB-E711-A2FA-02163E01A2D1.root',
+#       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/40000/C0EC07D4-D9DB-E711-A2FA-02163E01A2D1.root',
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/60000/24046F68-54E0-E711-A3F2-D4856444779A.root',
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/60000/34EDDE47-0BE1-E711-A5C6-984BE16440FC.root',
 #       '/store/data/Run2017B/SingleElectron/AOD/17Nov2017-v1/40000/78D54E40-EADB-E711-8940-02163E01A56D.root',
